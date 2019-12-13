933718d60acb60ccf9261e068df559059eb3ca81
Psage23 <pamelasage3@gmail.com>
2019-12-12 17:36:24 -0700

    building layout

---
+++ react-redux/src/components/CharacterCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Characters = () => {
+    return (
+        <div>
+            <p>player's photo</p>
+            <h1>Name</h1>
+            <p>Game series</p>
+        </div>
+    )
+}
+
+export default Characters;
